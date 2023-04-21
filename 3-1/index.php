0beb4c3df227a8530e5b2ac7d105c676ea225e87
miru-chan <130030179+miru-chan@users.noreply.github.com>
2023-04-21 11:12:39 +0900

    3-1課題提出
    3－1の課題です。

---
+++ 3-1/index.php
@@ -0,0 +1,21 @@
+<?php
+
+  for ($num = 1; $num <= 100; $num++) {
+
+    if ($num % 3 == 0 && $num % 5 == 0 ) {
+      echo "FizzBuzz";
+      echo "<br>";
+    } elseif ($num % 5 == 0) {
+      echo "Buzz";
+      echo "<Br>";
+    } elseif ($num % 3 == 0) {
+      echo "Fizz";
+      echo "<br>";
+    } else {
+      echo $num;
+      echo "<br>";
+    }
+
+  }
+
+?>
